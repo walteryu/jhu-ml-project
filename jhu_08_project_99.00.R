ec97d5a9a1d0114488afd7f038d57bc073a9ad37
wyu <kharma@gmail.com>
2021-01-03 10:11:08 -0800

    add ml module

---
+++ jhu_08_project_99.00.R
@@ -0,0 +1,11 @@
+# 99.00, final project: setup
+
+# load packages: machine learning models
+# reference: course quiz and assignments
+install.packages("caret", dependencies = TRUE)
+library(caret)
+
+install.packages("randomForest")
+library(randomForest)
+install.packages("gbm")
+library(gbm)
