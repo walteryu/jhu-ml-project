ae774fdd79c1d5db83da69828df1f93fcbd26daf
wyu <kharma@gmail.com>
2021-01-12 22:17:29 -0800

    organize modules

--- 03.00_appendix.R
+++
@@ -1,9 +0,0 @@
-# 03.00, final project: appendix
-
-# note: data appears to contain nulls upon initial inspection, so check for na count
-# reference: count null values within each column and output results
-# https://sebastiansauer.github.io/sum-isna/
-# print("columns containing null values (training data): ")
-# sapply(train_data, function(x) sum(is.na(x)))
-# print("columns containing null values (test data): ")
-# sapply(test_data, function(x) sum(is.na(x)))
